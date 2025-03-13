d6e6b8d86345ec87d5080dbab7998476ce2fb98c
Anacanedojesus <ana.canedo.jesus@escola.pr.gov.br>
2025-03-13 13:42:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let audios = [
-    {caminho:'Iframe BORA VIVER - Fiduma & Jeca (F&J 10 ANOS) [LFWH_sTlGQk].mp3', legenda:'Matuto de verdade'},
-    {caminho:'Iframe Cortando As BR - Fernando & Sorocaba, Fiduma & Jeca Ao Vivo em Jaguariúna [mS-PIgqcd-o].mp3', legenda:'Arash'},
-    {caminho:'Iframe Felipe & Rodrigo - Gosta de Rua (Ao Vivo Em Goiânia) #QuestãoDeTempo [ZsN_0_6yEXk].mp3', legenda:'Paula Fernandes'},
-    {caminho:'Iframe Israel & Rodolffo - Arruma Um Bão (Termômetro) [acfpoa3zsyE].mp3', legenda:'Hayit'},
-    {caminho:'Iframe IMAGINA A GENTE - Fiduma & Jeca , @FiorellaOficial (F&J 10 ANOS) [TWEsyJDO1mM].mp3', legenda:'Helena'},
-    {caminho:'Iframe Jorge & Mateus - Tem Que Sorrir (Clipe Oficial) [Álbum Tudo Em Paz] [AtHet50A5L4].mp3', legenda:'Paula Fernandes'},
-    {caminho:'Iframe Jorge & Mateus, Lauana Prado - Haverá Sinais (Clipe Oficial) [Muo3F10dAcw].mp3', legenda:'Paula Fernandes'},
-    {caminho:'Paula Fernandes.mp3', legenda:'Paula Fernandes'},
+    {caminho:'Iframe BORA VIVER - Fiduma & Jeca (F&J 10 ANOS) [LFWH_sTlGQk].mp3', legenda:'Bora viver'},
+    {caminho:'Iframe Cortando As BR - Fernando & Sorocaba, Fiduma & Jeca Ao Vivo em Jaguariúna [mS-PIgqcd-o].mp3', legenda:'cortando as br'},
+    {caminho:'Iframe Felipe & Rodrigo - Gosta de Rua (Ao Vivo Em Goiânia) #QuestãoDeTempo [ZsN_0_6yEXk].mp3', legenda:'gosta de rua'},
+    {caminho:'Iframe Israel & Rodolffo - Arruma Um Bão (Termômetro) [acfpoa3zsyE].mp3', legenda:'arruma um bão'},
+    {caminho:'Iframe IMAGINA A GENTE - Fiduma & Jeca , @FiorellaOficial (F&J 10 ANOS) [TWEsyJDO1mM].mp3', legenda:'imagina a gente'},
+    {caminho:'Iframe Jorge & Mateus - Tem Que Sorrir (Clipe Oficial) [Álbum Tudo Em Paz] [AtHet50A5L4].mp3', legenda:'tem que sorrir'},
+    {caminho:'Iframe Jorge & Mateus, Lauana Prado - Haverá Sinais (Clipe Oficial) [Muo3F10dAcw].mp3', legenda:'haverá sinais'},
+    {caminho:'Iframe Maiara & Maraisa - Vai Lá _ iMEMsidão [WuiNLPjTc_U].mp3', legenda:'vai la'},
     {caminho:'titanic.mp3', legenda:'Titanic'},
 ];
 
